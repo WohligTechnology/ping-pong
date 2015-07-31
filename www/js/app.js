30da174402bcc0614c50d4b7be88a302c5699635
jagruti181 <patiljagruti181@gmail.com>
2015-07-31 18:29:59 +0530

    change login

--- www/js/app.js
+++ www/js/app.js
@@ -91,7 +91,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         });
 
         // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('/login');
+        $urlRouterProvider.otherwise('/tab/dash');
 
     })
     .directive('barhighchart', function () {
