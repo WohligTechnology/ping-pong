ca2c0b25cf66d470dcca8777b498d3441912787c
jagruti181 <patiljagruti181@gmail.com>
2015-08-03 15:21:04 +0530

    cenge

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,4 @@
-
-var adminbase = "http://wohlig.co.in/attachbackend/";
+var adminbase = "http://www.wohlig.co.in/attachbackend/";
 var adminurl = adminbase + "index.php/json/";
 var adminhauth = adminbase + "index.php/hauth/";
 
@@ -7,68 +6,74 @@ var adminhauth = adminbase + "index.php/hauth/";
 angular.module('starter.services', [])
 
 .factory('MyServices', function($http) {
-  // Might use a resource here that returns a JSON array
+    // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
+    // Some fake testing data
+    var chats = [{
+        id: 0,
+        name: 'Ben Sparrow',
+        lastText: 'You on your way?',
+        face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+    }, {
+        id: 1,
+        name: 'Max Lynx',
+        lastText: 'Hey, it\'s me',
+        face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+    }, {
+        id: 2,
+        name: 'Adam Bradleyson',
+        lastText: 'I should buy a boat',
+        face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+    }, {
+        id: 3,
+        name: 'Perry Governor',
+        lastText: 'Look at my mukluks!',
+        face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+    }, {
+        id: 4,
+        name: 'Mike Harrington',
+        lastText: 'This is wicked good ice cream.',
+        face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+    }];
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
-    },
-	  checkLogin: function (type) {
-        return $http.get(adminhauth + "checkLogin", {
-            params: {
-                type: type
+    return {
+        all: function() {
+            return chats;
+        },
+        remove: function(chat) {
+            chats.splice(chats.indexOf(chat), 1);
+        },
+        get: function(chatId) {
+            for (var i = 0; i < chats.length; i++) {
+                if (chats[i].id === parseInt(chatId)) {
+                    return chats[i];
+                }
             }
-        });
-    },
-    getFacebookImages: function () {
-        return $http.get(adminhauth + "getFacebookImages");
-    },
-    getInstagramImages: function () {
-        return $http.get(adminhauth + "getInstagramImages");
-    },
-	  checkLogid: function(logid) {
-        return $http.get(adminhauth + "checkLogid",{params:{logid:logid}});
-    }
-	  
-  };
-});
+            return null;
+        },
+        checkLogin: function(type) {
+//		   return $http.get(adminhauth + "checkLogin?type="+type);
+		   return $http({
+                url: adminhauth + 'checkLogin',
+                method: "POST",
+                data: {
+                    'type': type
+                }
+            });
+        },
+        getFacebookImages: function() {
+            return $http.get(adminhauth + "getFacebookImages");
+        },
+        getInstagramImages: function() {
+            return $http.get(adminhauth + "getInstagramImages");
+        },
+        checkLogid: function(logid) {
+            return $http.get(adminhauth + "checkLogid", {
+                params: {
+                    logid: logid
+                }
+            });
+        }
 
+    };
+});
