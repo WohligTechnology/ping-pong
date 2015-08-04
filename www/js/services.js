05734c913fee845795c5e0c5ab06ced4a42ae18c
jagruti181 <patiljagruti181@gmail.com>
2015-08-04 15:24:32 +0530

    fit git

--- www/js/services.js
+++ www/js/services.js
@@ -1,42 +1,11 @@
-var adminbase = "http://www.wohlig.co.in/attachbackend/";
+
+var adminbase = "http://wohlig.co.in/attachbackend/";
 //var adminbase = "http://localhost/attachbackend/";
 var adminurl = adminbase + "index.php/json/";
 var adminhauth = adminbase + "index.php/hauth/";
 
-
 angular.module('starter.services', [])
-
-.factory('MyServices', function($http) {
-    // Might use a resource here that returns a JSON array
-
-    // Some fake testing data
-    var chats = [{
-        id: 0,
-        name: 'Ben Sparrow',
-        lastText: 'You on your way?',
-        face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-    }, {
-        id: 1,
-        name: 'Max Lynx',
-        lastText: 'Hey, it\'s me',
-        face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-    }, {
-        id: 2,
-        name: 'Adam Bradleyson',
-        lastText: 'I should buy a boat',
-        face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-    }, {
-        id: 3,
-        name: 'Perry Governor',
-        lastText: 'Look at my mukluks!',
-        face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-    }, {
-        id: 4,
-        name: 'Mike Harrington',
-        lastText: 'This is wicked good ice cream.',
-        face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-    }];
-
+.factory('MyServices', function ($http) {
     return {
         all: function() {
             return chats;
@@ -85,7 +54,10 @@ angular.module('starter.services', [])
             });
         },
         authenticate: function() {
-            return $http.get(adminurl + "authenticate");
+		   return $http({
+                url: adminurl + 'authenticate',
+                method: "POST"
+            });
         },
         checkLogid: function(logid) {
             return $http.get(adminhauth + "checkLogid", {
@@ -94,6 +66,5 @@ angular.module('starter.services', [])
                 }
             });
         }
-
     };
 });
